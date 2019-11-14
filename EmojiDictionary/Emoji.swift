c766719af6333c6f859330aaaf193d98163c4334
Julio Midence <jmidence@lcs.on.ca>
2019-11-13 21:01:36 -0500

    This is the model, 1/3 done

---
+++ EmojiDictionary/Emoji.swift
@@ -0,0 +1,19 @@
+//
+//  Emoji.swift
+//  EmojiDictionary
+//
+//  Created by Midence Rosario, Julio on 2019-11-13.
+//  Copyright © 2019 Julio Midence. All rights reserved.
+//
+
+import Foundation
+
+// Define the Emoji struct
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
+//
+//var example = Emoji(symbol: "🍕", name: "Pizza", description: "A delicious yet deadly combination of breed, tomato sauce, and cheese.", usage: "make when hungry")
